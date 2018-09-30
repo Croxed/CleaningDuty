0a45a582550a959623c17658bca88c6c3d2eb481
Croxed <oscar@wennergren.se>
2018-09-30 20:34:14 +0200

    Created wrapper class and FunctionalInterface for handling entities

---
+++ src/main/java/se/studentnatet/se/cleaningscheme/entities/Entity.java
@@ -0,0 +1,10 @@
+package se.studentnatet.se.cleaningscheme.entities;
+
+/**
+ * Only a wrapper class for other entities in the project.
+ * <p>
+ * In order for everything to work correctly, please note that classes that extends this class must implement a constructor with CSVRecord as parameter
+ */
+public abstract class Entity
+{
+}
